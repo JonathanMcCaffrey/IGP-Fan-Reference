2feea6292175fa89f476c942437e437fdf3ba54a
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2022-04-15 11:29:57 -0400

    style(RoadMap) Removing status and priority from roadmap. Changing its style

--- Model/RoadMap/ImmortalRoadMapModel.cs
+++ Model/RoadMap/ImmortalRoadMapModel.cs
@@ -21,7 +21,7 @@ public class ImmortalRoadMapModel
             Description =
                 @"The Calculator will be optimized to perform faster. It needs to be updated to consider training queue limits. Also, it needs error popups added for not enough ether, not enough supply, or no more interval time.",
             Priority = ReleasePriorityType.High,
-            Status = ReleaseStatusType.Planned
+            Status = ReleaseStatusType.In_Development
         },
         new ImmortalRoadMapModel
         {
@@ -45,7 +45,7 @@ public class ImmortalRoadMapModel
             Description =
                 @"There should be general notes on how to play Immortal. Nothing too extensive, but general faction and gameplay feel, like mentioning mechanics like Overgrowth for Aru and Wards for Q'Rath. Interesting but basic lore notes are also ideal, but all of these notes still need to be sortable in a method that feels natural, not a giant text bloat.",
             Priority = ReleasePriorityType.High,
-            Status = ReleaseStatusType.Planned
+            Status = ReleaseStatusType.In_Development
         },
         new ImmortalRoadMapModel
         {
@@ -62,22 +62,6 @@ public class ImmortalRoadMapModel
                 @"All patches should be tested via test automation, to avoid obvious bugs from getting into production. Add a informational test automation page to the Development section.",
             Priority = ReleasePriorityType.Medium,
             Status = ReleaseStatusType.Planned
-        },
-        new ImmortalRoadMapModel
-        {
-            Name = "Hot Key Reference",
-            Description =
-                @"Assuming the game continues to use Unreal-based hotkeys, there should be a reference for viewing and generating hotkey templates. Perhaps link to community hotkey files for people that do not wish to write their own.",
-            Priority = ReleasePriorityType.Low,
-            Status = ReleaseStatusType.Future_Possibility
-        },
-        new ImmortalRoadMapModel
-        {
-            Name = "Data Features",
-            Description =
-                @"Being able to save and load the state of the website would be cool. User's would keep all the data as JSON on their local machine, and just be able to load it when visiting the website. Being able to use the website offline would also be cool. People would only visit the live website to get updates. Perhaps offline website could do a check for this update version.",
-            Priority = ReleasePriorityType.Very_Low,
-            Status = ReleaseStatusType.Future_Possibility
         }
     };
 
