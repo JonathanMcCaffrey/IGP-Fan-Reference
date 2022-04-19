ab9bffc23fb91df11759f4387f18552eb2844f00
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2022-04-18 20:30:52 -0400

    fix(HarassCalculator) Travel time UX now makes sense. Fixed divide by zero errors

---
+++ Model/TravelTime.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Model;
+
+public class TravelTime
+{
+    public int Index { get; set; } = 0;
+    public int Value { get; set; } = 0;
+
+    public TravelTime(int index, int value)
+    {
+        Index = index;
+        Value = value;
+    }
+}
