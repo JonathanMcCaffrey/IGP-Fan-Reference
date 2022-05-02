8fa45196e9a5aa68ec954f9207d7bb4d0fa9b2c1
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2022-05-02 02:29:40 -0400

    Cors...

--- IGP/Program.cs
+++ IGP/Program.cs
@@ -22,7 +22,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<LazyAssemblyLoader>();
 
