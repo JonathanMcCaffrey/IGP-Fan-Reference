0caa61f42e53e6575dc2ca47002abcd856eec1a1
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2022-05-03 19:13:34 -0400

    feat(BuildCalc) Add +income to calculator info

--- Model/Economy/EconomyModel.cs
+++ Model/Economy/EconomyModel.cs
@@ -7,7 +7,9 @@ public class EconomyModel
 {
     public int Interval { get; set; } = 0;
     public float Alloy { get; set; } = 0;
+    public float AlloyIncome { get; set; } = 0;
     public float Ether { get; set; } = 0;
+    public float EtherIncome { get; set; } = 0;
     public float Pyre { get; set; } = 0;
     public int Supply { get; set; } = 0;
     public int WorkerCount { get; set; } = 6;
