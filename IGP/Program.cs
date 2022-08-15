8f10a98862f21bd538295b5600675eea677b748a
Jonathan McCaffrey <jonathan.mcaffrey@totaleto.com>
2022-08-15 02:03:52 -0400

    Downgrading .NET version to LTS

--- IGP/Program.cs
+++ IGP/Program.cs
@@ -25,7 +25,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<LazyAssemblyLoader>();
 
-builder.Services.AddProtectedBrowserStorage();
 
 builder.Services.AddLocalization();
 
