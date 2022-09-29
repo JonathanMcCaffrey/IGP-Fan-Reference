473c22ee2cf26f789182b26ff6dc0eef63802466
Jonathan McCaffrey <jonmcc.0723@gmail.com>
2022-09-28 21:36:00 -0400

    Removing Swagger

--- Server/Models/EntityWeapon.cs
+++ Server/Models/EntityWeapon.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Server.Model;
-using Microsoft.EntityFrameworkCore;
-
-namespace Server.Model;
+﻿namespace Server.Model;
 
 public partial class EntityWeapon
 {
@@ -36,62 +31,3 @@ public partial class EntityWeapon
     public virtual Entity? Entity { get; set; }
 }
 
-
-public static class EntityWeaponEndpoints
-{
-	public static void MapEntityWeaponEndpoints (this IEndpointRouteBuilder routes)
-    {
-        var group = routes.MapGroup("/api/EntityWeapon");
-        group.MapGet("/", async (JonathanMccaffreyContext db) =>
-        {
-            return await db.EntityWeapons.ToListAsync();
-        })
-        .WithName("GetAllEntityWeapons");
-
-        group.MapGet("/{id}", async  (int id, JonathanMccaffreyContext db) =>
-        {
-            return await db.EntityWeapons.FindAsync(id)
-                is EntityWeapon model
-                    ? Results.Ok(model)
-                    : Results.NotFound();
-        })
-        .WithName("GetEntityWeaponById");
-
-        group.MapPut("/{id}", async  (int id, EntityWeapon entityWeapon, JonathanMccaffreyContext db) =>
-        {
-            var foundModel = await db.EntityWeapons.FindAsync(id);
-
-            if (foundModel is null)
-            {
-                return Results.NotFound();
-            }
-            //update model properties here
-
-            await db.SaveChangesAsync();
-
-            return Results.NoContent();
-        })
-        .WithName("UpdateEntityWeapon");
-
-        group.MapPost("/", async (EntityWeapon entityWeapon, JonathanMccaffreyContext db) =>
-        {
-            db.EntityWeapons.Add(entityWeapon);
-            await db.SaveChangesAsync();
-            return Results.Created($"/api/EntityWeapon/{entityWeapon.Id}",entityWeapon);
-        })
-        .WithName("CreateEntityWeapon");
-
-        group.MapDelete("/{id}", async  (int id, JonathanMccaffreyContext db) =>
-        {
-            if (await db.EntityWeapons.FindAsync(id) is EntityWeapon entityWeapon)
-            {
-                db.EntityWeapons.Remove(entityWeapon);
-                await db.SaveChangesAsync();
-                return Results.Ok(entityWeapon);
-            }
-
-            return Results.NotFound();
-        })
-        .WithName("DeleteEntityWeapon");
-    }
-}
--- Server/Program.cs
+++ Server/Program.cs
@@ -7,9 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 builder.Services
     .AddGraphQLServer()
@@ -25,11 +22,9 @@ builder.Services.AddDbContext<JonathanMccaffreyContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapGraphQL();
 }
 
 app.UseHttpsRedirection();
@@ -37,12 +32,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapEntityWeaponEndpoints();
-
-app.MapEntityEndpoints();
-
-app.MapGraphQL();
 
 
 app.Run();
